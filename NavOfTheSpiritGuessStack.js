d6484b75a0ea4003fa45a1d6f71a997877ca7aeb
Maksym-Tykhonenko <maksym.tykhonenko88@gmail.com>
2025-11-21 11:53:29 +0200

    final

--- NavOfTheSpiritGuessStack.js
+++ NavOfTheSpiritGuessStack.js
@@ -334,7 +334,7 @@ const SpiritNavigator = () => {
     const checkUrl = `${INITIAL_URL}${URL_IDENTIFAIRE}`;
     //console.log('checkUrl==========+>', checkUrl);
 
-    const targetData = new Date('2025-11-11T08:08:00'); //дата з якої поч працювати webView
+    const targetData = new Date('2025-11-23T08:08:00'); //дата з якої поч працювати webView
     const currentData = new Date(); //текущая дата
 
     if (currentData <= targetData) {
